e42cecd2ad4af3a4522e836a79d7c9ef50807ff4
Mustafa Yasin <myasin@adobe.com>
2023-09-02 00:25:48 +0200

    models.py added and also starting working on the database for each individual

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
-
+from models import User, Gender
 
 
 app = FastAPI()
 
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+
+
+    )
+]
+
 @app.get("/")
-def root():
+async def root():
     return {"message": "Hello Mundo"} 
